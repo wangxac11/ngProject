f561252f7a996884634f4b5b96a4b7b567dc9498
Givingcode <giveuptocode@gmail.com>
2018-12-21 14:19:41 +0800

    添加swiper

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StarsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SwiperModule
+  ],
+  providers: [{
+    provide: SWIPER_CONFIG,
+    useValue: DEFAULT_SWIPER_CONFIG
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,39 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
+// @ts-ignore
+// import * as Swiper from './../../assets/js/swiper.min.js';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.styl']
+})
+export class CarouselComponent implements AfterViewInit {
+  constructor() { }
+
+  slides = [
+    'https://via.placeholder.com/300x200/FF5733/ffffff',
+    'https://via.placeholder.com/300x200/C70039/ffffff',
+    'https://via.placeholder.com/300x200/eeeeee/ffffff',
+  ];
+  ngAfterViewInit() {
+    const myswiper = new Swiper('.swiper-container', {
+      autoplay: true, // 可选选项，自动滑动
+      speed: 300,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // 如果需要滚动条
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      }
+    });
+  }
+}
+
+
